a8f9c6d247d58e2f7cce56732011839db2e79a25
xreotrace <marufbillah.dev@gmail.com>
2026-03-05 23:08:08 +0600

    implemented loadAllPlants() and displayPlants() through api

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,44 @@ const loadAllCategories = () => {
     .then((data) => displayCategories(data.categories));
 };
 
+const loadAllPlants = () => {
+  const allPlantUrl = "https://openapi.programming-hero.com/api/plants";
+
+  fetch(allPlantUrl)
+    .then((response) => response.json())
+    .then((data) => displayPlants(data.plants));
+};
+
+const displayPlants = (plantsData) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
+  plantsData.forEach((plant) => {
+    const newCard = document.createElement("div");
+    newCard.className = "card bg-base-100 shadow-sm border border-gray-100";
+    newCard.innerHTML = `
+        <figure class="px-4 pt-4">
+            <div class="w-full h-48 bg-gray-200 rounded-xl">
+                <img src="${plant.image}">
+            </div>
+        </figure>
+        <div class="card-body gap-1.5 p-5">
+            <h2 class="card-title text-lg font-semibold">${plant.name}</h2>
+            <p class="text-sm text-gray-500">${plant.description}</p>
+            <div class="flex justify-between items-center mt-2">
+                <span class="badge font-medium bg-emerald-50 text-green-700 border-none py-3 px-4">${plant.category}</span>
+                <span class="font-semibold text-lg">৳${plant.price}</span>
+            </div>
+            <div class="card-actions mt-2">
+                <button class="btn bg-green-700 hover:bg-green-800 text-white w-full border-none">Add to Cart</button>
+            </div>
+        </div>
+    `;
+
+    cardContainer.appendChild(newCard);
+  });
+};
+
 const displayCategories = (categoriesData) => {
   categoriesData.forEach((category) => {
     const categoryBtnContainer = document.getElementById(
@@ -28,3 +66,4 @@ const displayCategories = (categoriesData) => {
 };
 
 loadAllCategories();
+loadAllPlants();
